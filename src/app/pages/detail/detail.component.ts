d2401ac9be3ec28e60dac3e46422fc3df4e7e357
dingopes <olmyta@gmail.com>
2023-03-31 20:08:34 +0200

    proxy

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MediaCredits, MediaImages, MediaVideo } from 'src/app/models/media';
 import { Movie } from 'src/app/models/movie';
 import { MediaProviderLink } from 'src/app/models/providers';
 import { MoviesService } from 'src/app/services/movies.service';
 import { IMAGES_SIZES } from '../../constants/images-sizes';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
   detail: Movie | null = null;
   mediaVideos: MediaVideo[] = [];
   imagesSizes = IMAGES_SIZES;
@@ -20,22 +21,36 @@ export class DetailComponent implements OnInit {
   providersRent: MediaProviderLink[] = [];
   mediaImages: MediaImages | null = null;
   mediaCredits: MediaCredits | null = null;
+  similarMedia: Movie[] = [];
+
   constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(({ id }) => {
+    this.route.params.pipe(first()).subscribe(({ id }) => {
       this.getDetail(id);
       this.getMediaVideos(id);
       this.getMediaProviders(id, 'CZ');
       this.getMediaImages(id);
       this.getMediaCredits(id);
+      this.getMediaVideos(id);
+      this.getSimilarMedia(id);
     });
   }
 
+  ngOnDestroy(): void {
+    console.log('component destroyed');
+  }
   getDetail(id: string) {
     this.moviesService.getDetail(id).subscribe((detailData) => {
       this.detail = detailData;
       console.log(this.detail);
+    });
+  }
+
+  getSimilarMedia(id: string) {
+    this.moviesService.getSimilarMedia(id).subscribe((data) => {
+      this.similarMedia = data;
+      console.log('similar', this.similarMedia);
     });
   }
 
